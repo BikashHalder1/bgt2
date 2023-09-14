f24a96c99e0e687342b26b93bb87e7a567a0eb9e
BikashHalder1 <130992701+BikashHalder1@users.noreply.github.com>
2023-09-14 12:31:03 +0530

    Update stop.py

--- Bgt/plugins/stop.py
+++ Bgt/plugins/stop.py
@@ -5,17 +5,17 @@ from Bgt import app
 from Bgt.core.call import JavaCall
 from Bgt.utils.database import set_loop
 from Bgt.utils.inline.play import close_keyboard
-from Bgt.utils.decorators import AdminActual
+from Bgt.utils.decorators import AdminRightsCheck
 
 
-@app.on_message(filters.command(["stop", "end", "cstop", "cend"]) & filters.group & ~BANNED_USERS)
-@AdminActual
+@app.on_message(filters.command(["stop", "end", "cstop", "cend"]) & filters.group & ~BANNED_USERS & ~filters.forwarded)
+@AdminRightsCheck
 async def stop_music(cli, message: Message, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text("ᴇʀʀᴏʀ ! ᴡʀᴏɴɢ ᴜsᴀɢᴇ ᴏғ ᴄᴏᴍᴍᴀɴᴅ")
     await JavaCall.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
-        "**sᴛʀᴇᴀᴍ ᴇɴᴅᴇᴅ** \n│ \n└ʙʏ : {} ".format(message.from_user.mention),
+        "**sᴛʀᴇᴀᴍ ᴇɴᴅᴇᴅ** ʙʏ : {} ".format(message.from_user.mention),
         reply_markup=close_keyboard,
     )
