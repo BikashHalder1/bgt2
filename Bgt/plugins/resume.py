0dc41c0feb721396d5c9e3c3886a36b970e5b907
BikashHalder1 <130992701+BikashHalder1@users.noreply.github.com>
2023-09-14 12:10:38 +0530

    Update resume.py

--- Bgt/plugins/resume.py
+++ Bgt/plugins/resume.py
@@ -4,12 +4,12 @@ from pyrogram.types import Message
 from Bgt import app
 from Bgt.core.call import JavaCall
 from Bgt.utils.inline.play import close_keyboard
-from Bgt.utils.decorators import AdminActual
+from Bgt.utils.decorators import AdminRightsCheck
 from Bgt.utils.database import is_music_playing, music_on
 
 
-@app.on_message(filters.command(["resume", "cresume"]) & filters.group & ~BANNED_USERS)
-@AdminActual
+@app.on_message(filters.command(["resume", "cresume"]) & filters.group & ~BANNED_USERS & ~filters.forwarded)
+@AdminRightsCheck
 async def resume_com(cli, message: Message, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text("ᴇʀʀᴏʀ ! ᴡʀᴏɴɢ ᴜsᴀɢᴇ ᴏғ ᴄᴏᴍᴍᴀɴᴅ")
@@ -17,4 +17,4 @@ async def resume_com(cli, message: Message, chat_id):
         return await message.reply_text("ᴍᴜsɪᴄ ɪs ᴀʟʀᴇᴀᴅʏ ʀᴇsᴜᴍᴇᴅ​")
     await music_on(chat_id)
     await JavaCall.resume_stream(chat_id)
-    await message.reply_text("**sᴛʀᴇᴀᴍ ʀᴇsᴜᴍᴇᴅ** \n│ \n└ʙʏ : {} ".format(message.from_user.mention), reply_markup=close_keyboard)
+    await message.reply_text("**sᴛʀᴇᴀᴍ ʀᴇsᴜᴍᴇᴅ** ʙʏ : {} ".format(message.from_user.mention), reply_markup=close_keyboard)
