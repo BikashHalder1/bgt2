607abfd042a0c836f11017dd178fcc502dfcf1f4
BikashHalder1 <130992701+BikashHalder1@users.noreply.github.com>
2023-10-02 20:53:01 +0530

    Update welcome_.py

--- Bgt/plugins/welcome_.py
+++ Bgt/plugins/welcome_.py
@@ -1,1 +1,70 @@
 #bikash op
+
+import os, random, glob
+import numpy as np
+from config import LOG_GROUP_ID, BANNED_USERS
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ChatMemberUpdated
+from pyrogram.enums import ParseMode, ChatMemberStatus, ChatType
+from pyrogram.errors import MessageIdInvalid
+from Bgt import app
+from Bgt.utils.database import add_served_chat, blacklisted_chats, get_assistant
+from Bgt.utils.inline import start_pannel, close_keyboard
+
+
+
+
+@app.on_message(filters.new_chat_members & ~BANNED_USERS, group=13)
+async def welcome_bgt(c, message: Message):
+    chat_id = message.chat.id
+    await add_served_chat(chat_id)
+    if app.id in [user.id for user in message.new_chat_members]:
+        if chat_id in await blacklisted_chats():
+            await message.reply_text("**ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ᴄʜᴀᴛ**\n\nᴛʜɪs ᴄʜᴀᴛ ɪs ʙʟᴀᴄᴋʟɪsᴛ ғᴏʀ ᴜsɪɴɢ ᴛʜᴇ ʙᴏᴛ. ʀᴇǫᴜᴇsᴛ ᴀ sᴜᴅᴏ ᴜsᴇʀ ᴛᴏ ᴡʜɪᴛᴇʟɪsᴛ ʏᴏᴜʀ ᴄʜᴀᴛ sᴜᴅᴏ ᴜsᴇʀs [ʟɪsᴛ]({0}).".format(f"https://t.me/{app.username}?start=sudolist"))
+            return await app.leave_chat(chat_id)
+        userbot = await get_assistant(message.chat.id)
+        out = start_pannel()
+        await message.reply_text(
+            "Hᴇʏ ᴛʜɪs ɪs {0}\nᴀ ғᴀsᴛ ᴀɴᴅ ᴩᴏᴡᴇʀғᴜʟ ᴍᴜsɪᴄ ᴩʟᴀʏᴇʀ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴡᴇsᴏᴍᴇ ғᴇᴀᴛᴜʀᴇs\n\n๏ ᴀssɪsᴛᴀɴᴛ ᴜsᴇʀɴᴀᴍᴇ :- @{1}\n๏ ᴀssɪsᴛᴀɴᴛ ɪᴅ :- {2}".format(app.mention, userbot.username, userbot.id),
+            reply_markup=InlineKeyboardMarkup(out),
+        )
+        add_m = message.from_user.mention if message.from_user else "ᴜɴᴋɴᴏᴡɴ ᴜsᴇʀ"
+        add_id = message.from_user.username if message.from_user else "None"
+        title = message.chat.title
+        uname = f"@{message.chat.username}" if message.chat.username else "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
+        chat_id = message.chat.id
+        new = f"**✫** <b><u>ɴᴇᴡ ɢʀᴏᴜᴘ</u></b> **:**\n\n**ᴄʜᴀᴛ ɪᴅ :** {chat_id}\n**ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :** {uname}\n**ᴄʜᴀᴛ Tɪᴛʟᴇ :** {title}\n\n**ᴀᴅᴅᴇᴅ ʙʏ :** {add_m}\n**ᴀᴅᴅᴇᴅ ʙʏ :** @{add_id}"
+        chat = await app.get_chat(chat_id)
+        try:
+            link = chat.invite_link
+            if not link:
+                link = await app.export_chat_invite_link(chat_id)
+        except:
+            return
+        keyboard = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("Group Link", url=f"{link}")]
+            ]
+        )
+        await app.send_message(LOG_GROUP_ID, new, reply_markup=keyboard)
+            
+
+@app.on_message(~filters.me & ~filters.forwarded & ~BANNED_USERS, group=14)
+async def watcher(c, m: Message):
+    text = m.text
+    if m.chat.type == ChatType.PRIVATE:
+        try:
+            if text.startswith("/start"):
+                return
+            if text:
+                await c.forward_messages(LOG_GROUP_ID, m.chat.id, m.id)
+            else:
+                return
+        except AttributeError:
+            pass
+        except MessageIdInvalid:
+            pass
+    else:
+        return
+          
+        
