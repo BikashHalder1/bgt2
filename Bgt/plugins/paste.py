9d04c3d4b2792d18bd8afec8c384ebefe2331098
BikashHalder1 <130992701+BikashHalder1@users.noreply.github.com>
2023-11-11 10:52:41 +0530

    Update paste.py

--- Bgt/plugins/paste.py
+++ Bgt/plugins/paste.py
@@ -1,22 +1,40 @@
-from pyrogram import filters
-from pyrogram.types import Message
+import asyncio
+import os
+import re
 
+import aiofiles
+from pykeyboard import InlineKeyboard
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton
+
+from Bgt import aiohttpsession as session
 from Bgt import app
 from Bgt.utils.errors import capture_err
 
+pattern = re.compile(r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$")
 
-__MODULE__ = "Pᴀsᴛᴇ"
-__HELP__ = """
-⊱ /paste : Pᴀsᴛᴇ ғɪʟᴇ ᴛᴇxᴛ 
 
-"""
+async def isPreviewUp(preview: str) -> bool:
+    for _ in range(7):
+        try:
+            async with session.head(preview, timeout=2) as resp:
+                status = resp.status
+                size = resp.content_length
+        except asyncio.exceptions.TimeoutError:
+            return False
+        if status == 404 or (status == 200 and size == 0):
+            await asyncio.sleep(0.4)
+        else:
+            return True if status == 200 else False
+    return False
+
 
 @app.on_message(filters.command("paste"))
 @capture_err
 async def paste_func(_, message):
     if not message.reply_to_message:
-        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ sᴍᴀʟʟ ғɪʟᴇ ʟᴇss ᴛʜᴇɴ 1 ᴍʙ ᴜsɪɴɢ : /paste")
-    m = await message.reply_text("Pʟᴇᴀsᴇ ᴡᴀɪᴛ Pᴀsᴛɪɴɢ...........")
+        return await message.reply_text("Reply To A Message With /paste")
+    m = await message.reply_text("Pasting...")
     if message.reply_to_message.text:
         content = str(message.reply_to_message.text)
     elif message.reply_to_message.document:
@@ -32,7 +50,7 @@ async def paste_func(_, message):
     link = await paste(content)
     preview = link + "/preview.png"
     button = InlineKeyboard(row_width=1)
-    button.add(InlineKeyboardButton(text="Paste Link", url=link))
+    button.add(InlineKeyboardButton(text="• ᴘᴀsᴛᴇ ʟɪɴᴋ •", url=link))
 
     if await isPreviewUp(preview):
         try:
@@ -41,3 +59,10 @@ async def paste_func(_, message):
         except Exception:
             pass
     return await m.edit(link)
+    
+    
+__MODULE__ = "Pᴀsᴛᴇ"
+__HELP__ = """
+⊱ /paste : Pᴀsᴛᴇ ғɪʟᴇ ᴛᴇxᴛ 
+
+"""
