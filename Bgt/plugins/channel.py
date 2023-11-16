e8260934cdc91f5ffc86189049c2e743cb67d084
BikashHalder1 <130992701+BikashHalder1@users.noreply.github.com>
2023-11-16 22:25:30 +0530

    Update channel.py

--- Bgt/plugins/channel.py
+++ Bgt/plugins/channel.py
@@ -1,45 +1,67 @@
 from pyrogram import filters
-from pyrogram.types import Message
 from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
-from Bgt import app
+from pyrogram.types import Message
+
 from config import BANNED_USERS
+from strings import get_command
+from Bgt import app
 from Bgt.utils.database import set_cmode
 from Bgt.utils.decorators import AdminActual
 
 
-@app.on_message(filters.command(["channelplay"]) & filters.group & ~BANNED_USERS)
+
+@app.on_message(
+    filters.command(["channelplay"])
+    & filters.group
+    & ~BANNED_USERS
+)
 @AdminActual
 async def playmode_(client, message: Message):
     if len(message.command) < 2:
-        return await message.reply_text("ʏᴏᴜ ᴄᴀɴ ᴘʟᴀʏ ᴍᴜsɪᴄ ɪɴ ᴄʜᴀɴɴᴇʟs ꜰʀᴏᴍ ᴛʜɪs ᴄʜᴀᴛ [{0}] ᴛᴏ ᴀɴʏ ᴄʜᴀɴɴᴇʟ ᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ's ʟɪɴᴋᴇᴅ ᴄʜᴀɴɴᴇʟ.\n\n**ꜰᴏʀ ʟɪɴᴋᴇᴅ ᴄʜᴀɴɴᴇʟ:**\n/{1} `linked`\n\n**ꜰᴏʀ ᴀɴʏ ᴏᴛʜᴇʀ ᴄʜᴀɴɴᴇʟ:**\n/{1} [ᴄʜᴀɴɴᴇʟ ɪᴅ]".format(message.chat.title, ["channelplay"][0]))
+        return await message.reply_text(
+            "You can play music in channels from this chat[{0}] to any channel or your chat's linked channel.\n\n**For linked channel:**\n`/{1} linked`\n\n**For any other channel:**\n`/{1} [Channel ID]`".format(
+                message.chat.title, CHANNELPLAY_COMMAND[0]
+            )
+        )
     query = message.text.split(None, 2)[1].lower().strip()
     if (str(query)).lower() == "disable":
         await set_cmode(message.chat.id, None)
-        return await message.reply_text("ᴄʜᴀɴɴᴇʟ ᴩʟᴀʏ ᴅɪsᴀʙʟᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ ʙʏ {0} ɪɴ {1}".format(message.from_user.mention, message.chat.title))
+        return await message.reply_text("Channel Play Disabled")
     elif str(query) == "linked":
         chat = await app.get_chat(message.chat.id)
         if chat.linked_chat:
             chat_id = chat.linked_chat.id
             await set_cmode(message.chat.id, chat_id)
-            return await message.reply_text("ᴄʜᴀɴɴᴇʟ ᴅᴇꜰɪɴᴇᴅ ᴛᴏ {0}\n\nᴄʜᴀɴɴᴇʟ ɪᴅ: {1}".format(chat.linked_chat.title, chat.linked_chat.id))
+            return await message.reply_text(
+                "Channel Defined to {0}\n\n__Channel ID__: {1}".format(
+                    chat.linked_chat.title, chat.linked_chat.id
+                )
+            )
         else:
-            return await message.reply_text("ᴛʜɪs ᴄʜᴀᴛ ʜᴀs ɴᴏ ʟɪɴᴋᴇᴅ ᴄʜᴀɴɴᴇʟ")
+            return await message.reply_text("This chat has no linked channel.")
     else:
         try:
             chat = await app.get_chat(query)
-        except:
-            return await message.reply_text("ꜰᴀɪʟᴇᴅ ᴛᴏ ɢᴇᴛ ᴄʜᴀɴɴᴇʟ.\n\nᴍᴀᴋᴇ sᴜʀᴇ ʏᴏᴜ ʜᴀᴠᴇ ᴀᴅᴅᴇᴅ ʙᴏᴛ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴘʀᴏᴍᴏᴛᴇᴅ ɪᴛ ᴀs ᴀᴅᴍɪɴ.\nᴇᴅɪᴛ ᴏʀ ᴄʜᴀɴɢᴇ ᴄʜᴀɴɴᴇʟ ᴠɪᴀ /channelplay")
+        except Exception as e:
+            print(f"Error: {e}")
+            return await message.reply_text("Failed to get channel.\n\nMake sure you have added bot in your channel and promoted it as admin.\nEdit or Change channel via /channelplay")
         if chat.type != ChatType.CHANNEL:
-            return await message.reply_text("ᴏɴʟʏ ᴄʜᴀɴɴᴇʟs ᴀʀᴇ sᴜᴘᴘᴏʀᴛᴇᴅ")
+            return await message.reply_text("Only Channels are supported.")
         try:
-            admins = await app.get_chat_members(chat.id, filter=ChatMembersFilter.ADMINISTRATORS)
-        except:
-            return await message.reply_text("ꜰᴀɪʟᴇᴅ ᴛᴏ ɢᴇᴛ ᴄʜᴀɴɴᴇʟ.\n\nᴍᴀᴋᴇ sᴜʀᴇ ʏᴏᴜ ʜᴀᴠᴇ ᴀᴅᴅᴇᴅ ʙᴏᴛ ɪɴ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴘʀᴏᴍᴏᴛᴇᴅ ɪᴛ ᴀs ᴀᴅᴍɪɴ.\nᴇᴅɪᴛ ᴏʀ ᴄʜᴀɴɢᴇ ᴄʜᴀɴɴᴇʟ ᴠɪᴀ /channelplay")
-        for user in admins:
-            if user.status == ChatMemberStatus.OWNER:
-                cusername = user.user.username
-                cid = user.user.id
-        if cid != message.from_user.id:
-            return await message.reply_text("ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ʙᴇ ᴛʜᴇ **ᴏᴡɴᴇʀ** ᴏꜰ ᴛʜᴇ ᴄʜᴀɴɴᴇʟ [{0}] ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ɪᴛ ᴡɪᴛʜ ᴛʜɪs ɢʀᴏᴜᴘ.\n**ᴄʜᴀɴɴᴇʟ's ᴏᴡɴᴇʀ:** @{1}\n\nᴀʟᴛᴇʀɴᴀᴛɪᴠᴇʟʏ ʏᴏᴜ ᴄᴀɴ ʟɪɴᴋ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴛᴏ ᴛʜᴀᴛ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴛʜᴇɴ ᴛʀʏ ᴄᴏɴɴɴᴇᴄᴛɪɴɢ ᴡɪᴛʜ `/channelplay linked`".format(chat.title, cusername))
+            async for user in app.get_chat_members(
+                chat.id, filter=ChatMembersFilter.ADMINISTRATORS
+            ):
+                if user.status == ChatMemberStatus.OWNER:
+                    creatorusername = user.user.username
+                    creatorid = user.user.id
+        except Exception as e:
+            print(f"Error: {e}")
+            return await message.reply_text("Failed to get channel.\n\nMake sure you have added bot in your channel and promoted it as admin.\nEdit or Change channel via /channelplay")
+        if creatorid != message.from_user.id:
+            return await message.reply_text(
+                "You need to be the **Owner** of the channel[{0}] to connect it with this group.\n**Channel's Owner:** @{1}\n\nAlternatively you can link your group to that channel and then try connnecting with `/channelplay linked`".format(chat.title, creatorusername)
+            )
         await set_cmode(message.chat.id, chat.id)
-        return await message.reply_text("ᴄʜᴀɴɴᴇʟ ᴅᴇꜰɪɴᴇᴅ ᴛᴏ {0}\n\nᴄʜᴀɴɴᴇʟ ɪᴅ: {1}".format(chat.title, chat.id))
+        return await message.reply_text(
+            "Channel Defined to {0}\n\n__Channel ID__: {1}".format(chat.title, chat.id)
+        )
