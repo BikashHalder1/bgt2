34d2d496eabae21c24dfd3ff3fb24e3b10ce561b
ExoticHero <126850195+ExoticHero@users.noreply.github.com>
2024-05-04 11:33:46 +0530

    updated py-tgcalls decorator

--- Bgt/core/call.py
+++ Bgt/core/call.py
@@ -536,38 +536,44 @@ class Call(PyTgCalls):
             await self.five.start()   
             
     async def decorators(self):
-        @self.one.on_kicked()
-        @self.two.on_kicked()
-        @self.three.on_kicked()
-        @self.four.on_kicked()
-        @self.five.on_kicked()
-        @self.one.on_closed_voice_chat()
-        @self.two.on_closed_voice_chat()
-        @self.three.on_closed_voice_chat()
-        @self.four.on_closed_voice_chat()
-        @self.five.on_closed_voice_chat()
-        @self.one.on_left()
-        @self.two.on_left()
-        @self.three.on_left()
-        @self.four.on_left()
-        @self.five.on_left()
-
-        
+        @self.one.on_update(fl.chat_update(
+            ChatUpdate.Status.CLOSED_VOICE_CHAT | 
+            ChatUpdate.Status.LEFT_GROUP | 
+            ChatUpdate.Status.KICKED)
+        )
+        @self.two.on_update(fl.chat_update(
+            ChatUpdate.Status.CLOSED_VOICE_CHAT | 
+            ChatUpdate.Status.LEFT_GROUP | 
+            ChatUpdate.Status.KICKED)
+        )
+        @self.three.on_update(fl.chat_update(
+            ChatUpdate.Status.CLOSED_VOICE_CHAT | 
+            ChatUpdate.Status.LEFT_GROUP | 
+            ChatUpdate.Status.KICKED)
+        )
+        @self.four.on_update(fl.chat_update(
+            ChatUpdate.Status.CLOSED_VOICE_CHAT | 
+            ChatUpdate.Status.LEFT_GROUP | 
+            ChatUpdate.Status.KICKED)
+        )
+        @self.five.on_update(fl.chat_update(
+            ChatUpdate.Status.CLOSED_VOICE_CHAT | 
+            ChatUpdate.Status.LEFT_GROUP | 
+            ChatUpdate.Status.KICKED)
+        )
         async def stream_services_handler(ent, chat_id: int):
             await self.stop_stream(chat_id)
-
-
-        @self.one.on_stream_end()
-        @self.two.on_stream_end()
-        @self.three.on_stream_end()
-        @self.four.on_stream_end()
-        @self.five.on_stream_end()
         
-        async def stream_end_handler(client, update: Update):
+        
+        @self.one.on_update(fl.stream_end)
+        @self.two.on_update(fl.stream_end)
+        @self.three.on_update(fl.stream_end)
+        @self.four.on_update(fl.stream_end)
+        @self.five.on_update(fl.stream_end)
+        async def stream_end_handler1(client, update: Update):
             if not isinstance(update, StreamAudioEnded):
                 return
-            await self.change_stream(client, update.chat_id)
-        
+            await self.play(client, update.chat_id)
         
 
 JavaCall = Call()
