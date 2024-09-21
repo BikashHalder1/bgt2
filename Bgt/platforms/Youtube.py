1d0fe9a6148e4103f4599a039663f998ed53412e
BikashHalder1 <130992701+BikashHalder1@users.noreply.github.com>
2024-09-21 17:34:53 +0530

    Update Youtube.py

--- Bgt/platforms/Youtube.py
+++ Bgt/platforms/Youtube.py
@@ -413,3 +413,5 @@ class YouTubeAPI:
             direct = True
             downloaded_file = await loop.run_in_executor(None, audio_dl)
         return downloaded_file, direct
+
+YouTube = YouTubeAPI
