6cd8681284241157287c54aab27181f049dfbbfe
Uffy-xD <vivansingh8372@outlook.com>
2025-01-27 20:48:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,9 +41,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Bikashhalder1/bgt2")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/BikashHalder1/bgt2")
 
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Bikash")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "bikash")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
